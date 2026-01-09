10622892ba2c7fe0183c3de741a232bfcbb9a249
HALA AL-KHARRAZ <halakharraz04@gmail.com>
2026-01-09 19:58:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+
+using Microsoft.EntityFrameworkCore;
+using PersonsApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("Default")));
+
+var app = builder.Build();
+
+// Ensure database and tables are created on first run (no migrations required)
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Persons}/{action=Index}/{id?}");
+
+app.Run();
